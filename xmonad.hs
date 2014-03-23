98a74bc17de4bc197849790ca95d43ff499e4fb4
Daniel Collins <mail@dcollins.info>
2014-03-23 23:38:10 +1100

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,19 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad =<< xmobar defaultConfig
-         { modMask = mod4Mask
-         , terminal = "urxvt"
-         }
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
+myBar = "xmobar"
+
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig { modMask = mod4Mask,
+                           terminal = "urxvt",
+                           normalBorderColor = "#cccccc",
+                           focusedBorderColor = "#cd8b00" }
