55d5a07a3cad694da9bde8fb188038cbbd963cf7
Daniel Collins <mail@dcollins.info>
2015-12-02 22:57:55 +1100

    updated xmobar.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,21 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 import System.IO
 
-main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+main = xmonad =<< statusBar bar pp toggleStrutsKey conf
 
-myBar = "xmobar"
+bar = "xmobar"
 
-myPP = xmobarPP { ppCurrent = xmobarColor "#2aa198" "" . wrap "[" "]", ppTitle = xmobarColor "#2aa198" "" . shorten 50 }
+pp = xmobarPP
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
-myConfig = defaultConfig { modMask = mod4Mask,
-                           terminal = "urxvt",
-                           normalBorderColor = "#586e75",
-                           focusedBorderColor = "#d33682",
-                           borderWidth = 3 }
+isFloating w = gets (M.member w . W.floating . windowset)
 
+mouse :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
+mouse = \cf@(XConfig {modMask = modm}) ->
+     M.insert
+      (modm, button1)
+      (\w -> whenX (isFloating w) (mouseMoveWindow w))
+      (mouseBindings defaultConfig cf)
+
+conf = defaultConfig { modMask = mod4Mask,
+                       terminal = "urxvt",
+                       normalBorderColor = "#101010",
+                       focusedBorderColor = "#d0d0d0",
+                       startupHook = setWMName "LG3D",
+                       mouseBindings = mouse,
+                       borderWidth = 3 }
