400e76b9bdcbdfaa88bd5bfe0d09f7e0084bef1f
Daniel Collins <mail@dcollins.info>
2016-10-23 21:44:06 +1100

    fix a bug where xmobar would not be visible on first workspace

--- xmonad.hs
+++ xmonad.hs
@@ -2,35 +2,37 @@
 
 import XMonad
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 import System.IO
-
-main = xmonad =<< statusBar bar pp toggleStrutsKey conf
-
-bar = "xmobar"
-
-pp = xmobarPP
-
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 isFloating w = gets (M.member w . W.floating . windowset)
 
 mouse :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 mouse = \cf@(XConfig {modMask = modm}) ->
-     M.insert
+    M.insert
       (modm, button1)
       (\w -> whenX (isFloating w) (mouseMoveWindow w))
       (mouseBindings defaultConfig cf)
 
-conf = defaultConfig { modMask = mod4Mask,
-                       terminal = "urxvt",
-                       normalBorderColor = "#101010",
-                       focusedBorderColor = "#d0d0d0",
-                       startupHook = setWMName "LG3D",
-                       mouseBindings = mouse,
-                       borderWidth = 3 }
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $
+       defaultConfig
+         { modMask = mod4Mask,
+           terminal = "urxvt",
+           manageHook = manageDocks <+> manageHook defaultConfig,
+           layoutHook = avoidStruts $ layoutHook defaultConfig,
+           handleEventHook = handleEventHook defaultConfig <+> docksEventHook,
+           logHook = dynamicLogWithPP xmobarPP
+                      { ppOutput = hPutStrLn xmproc },
+           normalBorderColor = "#101010",
+           focusedBorderColor = "#d0d0d0",
+           startupHook = setWMName "LG3D",
+           mouseBindings = mouse,
+           borderWidth = 2 } `additionalKeys`
+                                [((mod4Mask, xK_b), sendMessage ToggleStruts)]
